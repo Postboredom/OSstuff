2f825b78dd0c0786e263422ac311c3e6e7d6127b
Postboredom <noah1010@me.com>
2018-03-27 11:51:22 -0700

    For Cassie

---
+++ Turnage/Animation with Blends/Assets/scripts/player.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public Animator anim;
+	public Rigidbody rbody;
+
+	private float inputH;
+	private float inputV;
+	private bool run;
+	private float runspeed = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		rbody = GetComponent<Rigidbody>();
+		run = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown("1"))
+		{
+			anim.Play("WAIT01",-1,0f);
+		}
+		if(Input.GetKeyDown("2"))
+		{
+			anim.Play("WAIT02",-1,0f);
+		}
+		if(Input.GetKeyDown("3"))
+		{
+			anim.Play("WAIT03",-1,0f);
+		}
+		if(Input.GetKeyDown("4"))
+		{
+			anim.Play("WAIT04",-1,0f);
+		}
+
+		if(Input.GetKey(KeyCode.Space))
+		{
+			anim.SetBool("jump",true);
+		}
+		else
+		{
+			anim.SetBool("jump", false);
+		}
+
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+			anim.SetBool("run",true);
+			runspeed = 3f;
+		}
+		else
+		{
+			anim.SetBool("run", false);
+			runspeed = 1f;
+		}
+
+		inputH = Input.GetAxis ("Horizontal");
+		inputV = Input.GetAxis ("Vertical");
+
+		anim.SetFloat("inputH",inputH);
+		anim.SetFloat("inputV",inputV);
+
+		float speed = 50f * runspeed;
+		float moveX = inputH*speed*Time.deltaTime;
+		float moveZ = inputV*speed*Time.deltaTime;
+
+		if(moveZ <= 0f)
+		{
+			moveX = 0f;
+		}
+
+		//another way to make character move is to set it's velocity vector
+		rbody.velocity = new Vector3(moveX,0f,moveZ);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Turnage/SuperHot 4/Assets/MyFPS.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyFPS : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat ("vertical", Input.GetAxis ("Vertical"));
+		anim.SetFloat ("horizontal", Input.GetAxis ("Horizontal"));	
+	}
+}
